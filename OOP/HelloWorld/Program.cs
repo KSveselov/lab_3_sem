284e4a052d510c996d1a5b624edbccf4ae6e8c24
Kirill Veselov <beselov20062011@yandex.ru>
2026-09-03 17:19:41 +0300

    Решение 1 модуля

--- OOP/HelloWorld/Program.cs
+++ OOP/HelloWorld/Program.cs
@@ -6,7 +6,19 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(GetLastHalf("I love CSharp!"));
+            Console.WriteLine(GetLastHalf("1234567890"));
+            Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
+        }
+
+        static string GetLastHalf(string text)
+        {   
+            int start = text.Length/2;
+            int end = text.Length;
+
+            string second_part = text.Substring(start,end-start);
+            return second_part.Replace(" ", "");
+
         }
     }
 }
