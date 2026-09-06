df7614a8861a856d6dd230b959400217d645f23d
Kirill Veselov <beselov20062011@yandex.ru>
2026-09-06 18:11:10 +0300

    Иниц. новый проект

---
+++ OOP/Percent/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Decode("1..02.6"));
+            Console.WriteLine(Decode("1010"));
+        }
+
+        static int Decode(string arg)
+        {
+            return int.Parse(arg.Replace(".", ""))%1024;
+        }
+    }
+}
